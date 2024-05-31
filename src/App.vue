2044521ff067dd512b64ac989c3c9f6118442bbc
koxuan <g@vkabc.me>
2024-05-31 19:52:11 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
+
+
+const link = ref('');
+const address = ref('');
+
+const add = (address2:string) => {
+  link.value = 'https://zetachain-athens-3.blockscout.com/address/'+address.value;
+
+};
+
+</script>
+
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="Zeta Tracker" />
+    </div>
+    <div class="flex gap-x-2 mb-8">
+      <input v-model="address" type="text" id="large-input" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+
+
+
+
+      <button @click="add" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</button>
+    </div>
+
+    <a  target="_blank" :href="link" class="mt-5">
+      {{link}}
+    </a>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
